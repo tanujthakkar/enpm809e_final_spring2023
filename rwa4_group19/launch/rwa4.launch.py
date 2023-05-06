c6f80f976d291e869182bd2d47fa35b9e3924ed4
Tanuj Thakkar <tanujthakkar720@gmail.com>
2023-05-06 17:30:04 -0400

    Added config folder and order.yaml

--- rwa4_group19/launch/rwa4.launch.py
+++ rwa4_group19/launch/rwa4.launch.py
@@ -1,7 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     '''
@@ -11,10 +11,17 @@ def generate_launch_description():
         LaunchDescription: LaunchDescription object
     '''
     launch_description = LaunchDescription()
+
+    config = os.path.join(get_package_share_directory("rwa4_group19"), 
+                          'config', 
+                          'order.yaml')
     
     rwa4_node = Node(
         package="rwa4_group19",
-        executable="rwa4")
+        executable="rwa4",
+        parameters=[config],
+        output="screen",
+    )
     
     launch_description.add_action(rwa4_node)
     return launch_description
